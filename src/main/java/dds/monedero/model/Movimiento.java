43464f74f5aedc5afcdfbab4bf41a198ef8e606d
gonzalemi <emiliano.gonzalez@hotmail.com>
2020-05-03 18:38:53 -0300

    refactor temporary field: saldo

--- src/main/java/dds/monedero/model/Movimiento.java
+++ src/main/java/dds/monedero/model/Movimiento.java
@@ -44,15 +44,6 @@ public class Movimiento {
   }
 
   public void agregateA(Cuenta cuenta) {
-    cuenta.setSaldo(calcularValor(cuenta));
-    cuenta.agregarMovimiento(fecha, monto, esDeposito);
-  }
-
-  public double calcularValor(Cuenta cuenta) {
-    if (esDeposito) {
-      return cuenta.getSaldo() + getMonto();
-    } else {
-      return cuenta.getSaldo() - getMonto();
-    }
+     cuenta.agregarMovimiento(fecha, monto, esDeposito);
   }
 }
